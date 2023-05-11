ed8eef8b70e166b83c0d34b5af106e08ba40cbc6
Allan Szewczyk <jfa_nakata@hotmail.com>
2023-05-11 17:07:10 +0900

    Remove temp files after download and add comments

--- download.sh
+++ download.sh
@@ -1,23 +1,26 @@
-
+# make environmental variables available
 . .env
 
+# create theme directory
 mkdir -p $TMP_DIR
 
+# fetch raw theme data from FusionAuth
 curl -s -H 'Authorization: '$API_KEY  $FUSIONAUTH_URL/api/theme/$THEME_ID > $TMP_DIR/themeout.json
-cat $TMP_DIR/themeout.json |jq '.theme.templates' > $TMP_DIR/templates.json
 
-# splits template files
+# create template files from raw data
+cat $TMP_DIR/themeout.json |jq '.theme.templates' > $TMP_DIR/templates.json
 node splitfiles.js templates.json .ftl
 
+# create default messages from raw data
 cat $TMP_DIR/themeout.json |jq '.theme.defaultMessages'  |sed 's/^"//' |sed 's/"$//' |sed 's/\\"/"/g' |sed 's/\\\\/\\/g' |node convert-newlines.js > $TMP_DIR/defaultMessages.txt
 
+# create localized messages from raw data
 cat $TMP_DIR/themeout.json |jq '.theme.localizedMessages' > $TMP_DIR/localizedMessages.json
 node splitfiles.js localizedMessages.json .txt
 
+# create stylesheet from raw data
 # empty string if null https://stackoverflow.com/questions/53135035/jq-returning-null-as-string-if-the-json-is-empty
 cat $TMP_DIR/themeout.json |jq '.theme.stylesheet // empty'  |sed 's/^"//' |sed 's/"$//' |node convert-newlines.js > $TMP_DIR/styles.css
 
-#curl -H 'Authorization: '$API_KEY  $FUSIONAUTH_URL/api/theme/$THEME_ID|jq '.theme.localizedMessages' > $TMP_DIR/localizedMessages.properties
-
-
-#rm $TMP_DIR/templates.json $TMP_DIR/themeout.json
+# clean up no longer needed files
+rm $TMP_DIR/themeout.json $TMP_DIR/templates.json $TMP_DIR/localizedMessages.json
